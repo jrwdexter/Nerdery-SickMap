b2ecb0ce1ef4b89ebafae7f104aaff6b49b12b9e
Jonathan Dexter <jdexter@nerdery.com>
2014-01-22 12:18:29 -0600

    Updated the mail module to return less items from fewer days.  Improves performance.

--- SickMap/Mail.fs
+++ SickMap/Mail.fs
@@ -7,7 +7,7 @@ open ImapX.Authentication
 
 module Mail =
   let GetNewestEmailMatching (name:string) (client:ImapClient) =
-    let sinceDate = DateTime.Now.AddDays(-3.0).ToString("dd-MMM-yyyy")
+    let sinceDate = DateTime.Now.AddDays(-1.0).ToString("dd-MMM-yyyy")
     let query = String.Format("SINCE \"{0}\" SUBJECT \"{1}\"", sinceDate, name)
-    client.Folders.All.Search(query, Enums.MessageFetchMode.Full, 50)
+    client.Folders.All.Search(query, Enums.MessageFetchMode.Full, 10)
     |> Seq.last
